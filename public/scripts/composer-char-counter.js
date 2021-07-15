b02743b93ca4d636927d3d97165622ab1682969f
Mona Waseem <monaqwaseem@gmail.com>
2021-07-15 04:57:55 +0000

    Create the tweets.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
   // --- our code goes here ---
   const $textarea = $("form textarea");
-
-
-  //   $("#btn").on('click', function() {
+  
+  $textarea.on("input", function(event) {
+    const count = $(this).val().length;
+    const $counter = $(this).next().find(".counter");
+    $counter.text(140 - count);
+    // console.log(counter.text);
+    if (count > 140) {
+      $counter.css("color", "red");
+    }
+  });
+});
+//   $("#btn").on('click', function() {
 //     console.log(this); //The this keyword is a reference to the button
 //   });
 
-$("#btn").on('click', () => {
-  console.log(this); //The this keyword here refers to something else!
-});
+// $("#btn").on('click', () => {
+//   console.log(this); //The this keyword here refers to something else!
+// });
