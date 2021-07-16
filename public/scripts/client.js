744aaf202062ee780847a2add3c4834203d003ae
Mona Waseem <monaqwaseem@gmail.com>
2021-07-16 04:48:12 +0000

    Create dynamic tweet.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,5 +19,21 @@ let tweetData = [
 ];
 
 const createTweetElement = (tweetObject) => {
-  
+  const tweetTemplate = `<div class='tweet-container'>
+  <div class='useravatar'>
+    <p><img class='reincarnation' src ='${tweetObject["user"].avatars}'/></p>
+    <p class='username'>${tweetObject["user"].name}</p>
+    <p class="handle">${tweetObject["user"].handle}</p>
+  </div>
+   <p class='tweetcontent'>${escape(tweetObject["content"].text)}</p>  
+  <hr>
+  <div class='date-flags-container'>
+    <p class='datecreated'>${timeago.format(tweetObject["created_at"])}</p>
+    <div class='flags-container'>
+      <p class="like"><i class="fas fa-heart"></i></p>
+      <p class='retweet'><i class="fas fa-retweet"></i></p>
+      <p class="flag"><i class="fas fa-flag"></i></p>
+    </div>
+  </div>
+</div>`;
 };
